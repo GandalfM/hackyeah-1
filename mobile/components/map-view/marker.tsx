1047dc547bdcfbe6d68f0a82895927899c072a28
Bartlomiej Lewandowski <bartlomiej@digitaltoucan.com>
2019-09-15 11:38:10 +0200

    custom marker ;)

--- mobile/components/map-view/marker.tsx
+++ mobile/components/map-view/marker.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Marker as MapMarker } from 'react-native-maps';
+import { Image } from 'react-native';
 
-export default function Marker({ location, onPress, isSelected }) {
+const markerImg = require('../../assets/marker.png');
+const img = <Image style={{width: 15, height: 33}} source={markerImg}/>;
+export default function Marker({location, onPress, isSelected}) {
     return <MapMarker
-        coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+        coordinate={{latitude: location.latitude, longitude: location.longitude}}
         onPress={onPress}
-        pinColor={isSelected ? "aqua" : "navy"}
+        pinColor={isSelected ? "#2d557a" : "#4e9bc6"}
     >
+
+        {img}
     </MapMarker>
 }
